7e728ae98e8ac18c4856b5972b42dc6d705cfe85
pananfly <906792655@qq.com>
2020-04-06 23:30:53 +0800

    Update test sum fun

--- KTLearnGrammer/app/src/test/java/com/pananfly/learning/kt/ktlearngrammer/BaseGrammerUnitTest.kt
+++ KTLearnGrammer/app/src/test/java/com/pananfly/learning/kt/ktlearngrammer/BaseGrammerUnitTest.kt
@@ -5,11 +5,15 @@ import org.junit.Test
 class BaseGrammerUnitTest {
 
     fun test_fun_sum(a: Int, b: Int): Int {
-        return a + b;
+        return a + b
     }
+
+    fun test_fun_sum2(a: Int, b: Int) = a + b;
 
     @Test
     fun test_fun() {
+        //page 72
         println("3 + 5 = " + test_fun_sum(3 , 5))
+        println("4 + 5 = ${test_fun_sum2(4 , 5)}")
     }
 }
