63f06f82e01eb382bd87ee3c57f528f911bf1182
pananfly <906792655@qq.com>
2020-07-09 00:28:33 +0800

    Update coroutines content (context).

--- KTLearnGrammer/app/src/test/java/com/pananfly/learning/kt/ktlearngrammer/BaseCoroutinesUnitTest.kt
+++ KTLearnGrammer/app/src/test/java/com/pananfly/learning/kt/ktlearngrammer/BaseCoroutinesUnitTest.kt
@@ -489,5 +489,247 @@ class BaseCoroutinesUnitTest {
 
     }
 
+    @Test
+    fun testContext() = runBlocking<Unit> {
+        GlobalScope.launch { // 跟Default一样
+            println("GlobalScope launch      : I'm working in thread ${Thread.currentThread().name}")
+        }
+        launch { // 运行在父协程的上下文中，即 runBlocking 主协程
+            println("main runBlocking      : I'm working in thread ${Thread.currentThread().name}")
+        }
+        launch(Dispatchers.Unconfined) { // 不受限的——将工作在主线程中
+            println("Unconfined            : I'm working in thread ${Thread.currentThread().name}")
+        }
+        launch(Dispatchers.Default) { // 将会获取默认调度器
+            println("Default               : I'm working in thread ${Thread.currentThread().name}")
+        }
+        launch(newSingleThreadContext("MyOwnThread")) { // 将使它获得一个新的线程
+            println("newSingleThreadContext: I'm working in thread ${Thread.currentThread().name}")
+        }
 
+        """
+            GlobalScope launch      : I'm working in thread DefaultDispatcher-worker-1 @coroutine#2
+            Unconfined            : I'm working in thread main @coroutine#4
+            Default               : I'm working in thread DefaultDispatcher-worker-1 @coroutine#5
+            newSingleThreadContext: I'm working in thread MyOwnThread @coroutine#6
+            main runBlocking      : I'm working in thread main @coroutine#3
+        """.trimIndent()
+    }
+
+    @Test
+    fun testContext2() = runBlocking<Unit> {
+        println("main-===      : I'm working in thread ${Thread.currentThread().name}")
+        launch(Dispatchers.Unconfined) { // 非受限的——将和主线程一起工作
+            println("Unconfined      : I'm working in thread ${Thread.currentThread().name}")
+            delay(500)
+            println("Unconfined      : After delay in thread ${Thread.currentThread().name}")
+        }
+        launch { // 父协程的上下文，主 runBlocking 协程
+            println("main runBlocking: I'm working in thread ${Thread.currentThread().name}")
+            delay(1000)
+            println("main runBlocking: After delay in thread ${Thread.currentThread().name}")
+        }
+
+        // Unconfined协程中被挂起后使用的是默认的执行者线程中恢复执行
+        // 非受限的调度器是一种高级机制，可以在某些极端情况下提供帮助而不需要调度协程以便稍后执行或产生不希望的副作用，
+        // 因为某些操作必须立即在协程中执行。 非受限调度器不应该在通常的代码中使用。
+        """
+            Unconfined      : I'm working in thread main @coroutine#2
+            main runBlocking: I'm working in thread main @coroutine#3
+            Unconfined      : After delay in thread kotlinx.coroutines.DefaultExecutor @coroutine#2
+            main runBlocking: After delay in thread main @coroutine#3
+        """.trimIndent()
+    }
+
+    @Test
+    fun testContext3() = runBlocking<Unit> {
+        // 使用标准函数use释放线程
+        newSingleThreadContext("Ctx1").use { ctx1 ->
+            newSingleThreadContext("Ctx2").use { ctx2 ->
+                runBlocking(ctx1) {
+                    println("Started in ctx1 in thread ${Thread.currentThread().name}")
+                    withContext(ctx2) {
+                        println("Working in ctx2 in thread ${Thread.currentThread().name}")
+                    }
+                    println("Back to ctx1 in thread ${Thread.currentThread().name}")
+                }
+            }
+        }
+
+        """
+            Started in ctx1 in thread Ctx1 @coroutine#2
+            Working in ctx2 in thread Ctx2 @coroutine#2
+            Back to ctx1 in thread Ctx1 @coroutine#2
+        """.trimIndent()
+    }
+
+    @Test
+    fun testContext4() = runBlocking<Unit> {
+        println("My job is ${coroutineContext[Job]}")
+        println("My job isActive ${coroutineContext[Job]?.isActive}")
+        println("My job isCompleted ${coroutineContext[Job]?.isCompleted}")
+        println("My job isCancelled ${coroutineContext[Job]?.isCancelled}")
+
+        // 检索协程上下文中job的状态
+        """
+            My job is "coroutine#1":BlockingCoroutine{Active}@34033bd0
+            My job isActive true
+            My job isCompleted false
+            My job isCancelled false
+        """.trimIndent()
+    }
+
+    @Test
+    fun testContext5() = runBlocking<Unit> {
+
+        // 启动一个协程来处理某种传入请求（request）
+        val request = launch {
+            // 孵化了两个子作业, 其中一个通过 GlobalScope 启动
+            GlobalScope.launch {
+                println("job1: I run in GlobalScope and execute independently!")
+                delay(1000)
+                println("job1: I am not affected by cancellation of the request")
+            }
+            // 另一个则承袭了父协程的上下文
+            launch {
+                delay(100)
+                println("job2: I am a child of the request coroutine")
+                delay(1000)
+                println("job2: I will not execute this line if my parent request is cancelled")
+            }
+        }
+        delay(500)
+        request.cancel() // 取消请求（request）的执行
+        delay(1000) // 延迟一秒钟来看看发生了什么
+        println("main: Who has survived request cancellation?")
+
+        // GlobalScope子协程在父协程被取消时不影响其执行
+        """
+            job1: I run in GlobalScope and execute independently!
+            job2: I am a child of the request coroutine
+            job1: I am not affected by cancellation of the request
+            main: Who has survived request cancellation?
+        """.trimIndent()
+    }
+
+    @Test
+    fun testContext6() = runBlocking<Unit> {
+        // 启动一个协程来处理某种传入请求（request）
+        val request = launch {
+            repeat(3) { i -> // 启动少量的子作业
+                launch  {
+                    delay((i + 1) * 200L) // 延迟 200 毫秒、400 毫秒、600 毫秒的时间
+                    println("Coroutine $i is done")
+                }
+            }
+            println("request: I'm done and I don't explicitly join my children that are still active")
+        }
+        request.join() // 等待请求的完成，包括其所有子协程
+        println("Now processing of the request is complete")
+    }
+
+    fun log(msg: String) = println("[${Thread.currentThread().name}] $msg")
+
+    @Test
+    fun testContext7() = runBlocking<Unit>(CoroutineName("main")) {
+        log("Started main coroutine")
+        // run two background value computations
+        val v1 = async(CoroutineName("v1coroutine")) {
+            delay(500)
+            log("Computing v1")
+            252
+        }
+        val v2 = async(CoroutineName("v2coroutine")) {
+            delay(1000)
+            log("Computing v2")
+            6
+        }
+        log("The answer for v1 / v2 = ${v1.await() / v2.await()}")
+
+        //自定义协程名称
+        """
+            [main @main#1] Started main coroutine
+            [main @v1coroutine#2] Computing v1
+            [main @v2coroutine#3] Computing v2
+            [main @main#1] The answer for v1 / v2 = 42
+        """.trimIndent()
+    }
+
+    @Test
+    fun testContext8() = runBlocking<Unit> {
+        // 组合上下文元素
+        launch(Dispatchers.Default + CoroutineName("testName")) {
+            println("I'm working in thread ${Thread.currentThread().name}")
+        }
+
+        """
+            I'm working in thread DefaultDispatcher-worker-1 @testName#2
+        """.trimIndent()
+    }
+
+    class MainScopeTest {
+//        private val mainScope = MainScope() // MainScope需要用到安卓的东西，在这里执行不了，会报异常
+        private val mainScope = CoroutineScope(Dispatchers.Default)
+
+        fun destroy() {
+            mainScope.cancel()
+        }
+
+        fun doSomething() {
+            // 在示例中启动了 10 个协程，且每个都工作了不同的时长
+            repeat(10) { i ->
+                mainScope.launch {
+                    delay((i + 1) * 200L) // 延迟 200 毫秒、400 毫秒、600 毫秒等等不同的时间
+                    println("Coroutine $i is done")
+                }
+            }
+        }
+    }
+
+    @Test
+    fun testContext9() = runBlocking<Unit> {
+        val activity = MainScopeTest()
+        activity.doSomething() // 运行测试函数
+        println("Launched coroutines")
+        delay(500L) // 延迟半秒钟
+        println("Destroying activity!")
+        activity.destroy() // 取消所有的协程
+        delay(1000) // 为了在视觉上确认它们没有工作
+
+        """
+            Launched coroutines
+            Coroutine 0 is done
+            Coroutine 1 is done
+            Destroying activity!
+        """.trimIndent()
+    }
+
+    val threadLocal = ThreadLocal<String?>() // declare thread-local variable
+
+    @Test
+    fun testContext10() = runBlocking<Unit> {
+        threadLocal.set("main")
+        println("Pre-main, current thread: ${Thread.currentThread()}, thread local value: '${threadLocal.get()}'")
+        val job = launch(Dispatchers.Default + threadLocal.asContextElement(value = "launch")) {
+            println("Launch start, current thread: ${Thread.currentThread()}, thread local value: '${threadLocal.get()}'")
+            yield()
+            println("After yield, current thread: ${Thread.currentThread()}, thread local value: '${threadLocal.get()}'")
+            threadLocal.ensurePresent()
+        }
+        job.join()
+        println("Post-main, current thread: ${Thread.currentThread()}, thread local value: '${threadLocal.get()}'")
+
+        """
+            ThreadLocal 具有一流的支持，可以与任何 kotlinx.coroutines 提供的原语一起使用。 但它有一个关键限制，
+            即：当一个线程局部变量变化时，则这个新值不会传播给协程调用者（因为上下文元素无法追踪所有 ThreadLocal 对象访问），
+            并且下次挂起时更新的值将丢失。
+        """.trimIndent()
+
+        """
+            Pre-main, current thread: Thread[main @coroutine#1,5,main], thread local value: 'main'
+            Launch start, current thread: Thread[DefaultDispatcher-worker-1 @coroutine#2,5,main], thread local value: 'launch'
+            After yield, current thread: Thread[DefaultDispatcher-worker-1 @coroutine#2,5,main], thread local value: 'launch'
+            Post-main, current thread: Thread[main @coroutine#1,5,main], thread local value: 'main'
+        """.trimIndent()
+    }
 }
